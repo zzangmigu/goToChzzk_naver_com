926917948c63ee60f0cbcab4a71cc1f3af9633fa
ZzangMiGu <77621529+zzangmigu@users.noreply.github.com>
2024-05-14 19:18:39 +0900

    [Bronze V] Title: 윤년, Time: 4 ms, Memory: 896 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var year int
+	fmt.Scanln(&year)
+	if year%4 == 0 && year%100 != 0 || year%400 == 0 {
+		fmt.Println("1")
+	} else {
+		fmt.Println("0")
+	}
+
+}
