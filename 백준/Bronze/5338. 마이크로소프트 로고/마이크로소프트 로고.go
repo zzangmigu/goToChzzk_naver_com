5edfa8293319d418697d971f205467a33faeebe4
ZzangMiGu <77621529+zzangmigu@users.noreply.github.com>
2024-06-01 18:57:45 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 4 ms, Memory: 824 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("       _.-;;-._")
+	fmt.Println("'-..-'|   ||   |")
+	fmt.Println("'-..-'|_.-;;-._|")
+	fmt.Println("'-..-'|   ||   |")
+	fmt.Println("'-..-'|_.-''-._|")
+
+}
