244803b04500250cb787aedede5aaea8cc494a0e
ZzangMiGu <77621529+zzangmigu@users.noreply.github.com>
2024-07-09 11:14:19 +0900

    [Bronze III] Title: 경기 결과, Time: 544 ms, Memory: 6264 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/5523. 경기 결과/경기 결과.go
+++ 백준/Bronze/5523. 경기 결과/경기 결과.go
@@ -1,21 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
 	var n int
-	fmt.Scan(&n)
+	fmt.Fscan(reader, &n)
 	var aWin, bWin int
 	for i := 0; i < n; i++ {
 		var a, b int
-		fmt.Scan(&a, &b)
+		fmt.Fscan(reader, &a, &b)
 		if a > b {
 			aWin++
 		} else if a < b {
 			bWin++
 		}
 	}
-	fmt.Println(aWin, bWin)
+	fmt.Fprint(writer, aWin, bWin)
 }
