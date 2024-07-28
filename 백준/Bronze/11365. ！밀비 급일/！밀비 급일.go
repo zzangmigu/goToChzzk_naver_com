be0f93d97ea554640932826036d97943af9ad071
ZzangMiGu <77621529+zzangmigu@users.noreply.github.com>
2024-07-28 19:52:50 +0900

    [Bronze IV] Title: !밀비 급일, Time: 4 ms, Memory: 868 KB -BaekjoonHub

---
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		text := scanner.Text()
+		if text == "END" {
+			break
+		}
+		fmt.Println(reverse(text))
+	}
+}
+
+func reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
