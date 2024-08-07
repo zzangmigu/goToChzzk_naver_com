37acbe611755b7f206a57227f09ab35e4e7b5fb9
ZzangMiGu <77621529+zzangmigu@users.noreply.github.com>
2024-08-07 23:39:19 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 4 ms, Memory: 868 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+
+    "fmt"
+
+)
+
+func main() {
+
+    var k, q, r, b, n, p int
+
+    fmt.Scan(&k, &q, &r, &b, &n, &p)
+
+    fmt.Println(1-k, 1-q, 2-r, 2-b, 2-n, 8-p)
+
+}
+
